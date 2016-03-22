192ef93091ce903b2825840d3a12d7cfeb0bb1dc
Andrew Bae <andrewbae94@gmail.com>
2016-03-22 11:42:59 -0400

    Get Nav

--- js/getNav.js
+++ js/getNav.js
@@ -1,12 +1,17 @@
-$.get("http://abae9.github.io/partials/nav.html", function(data) {
+//get the file (replace with your own url)
+$.get("http://abae9.github.io/otherProjects/partials/nav.html", function(data) {
 
   $(document).ready(function() {
-	//check if this document has .container
-	var container = $(".container");
-	//if so add nav.html contents to the top
-	if (container) {
-  	container.prepend(data)
-  	//container.fadeIn();
-	}
+
+    //check if this document has .container
+    var container = $(".container");
+
+    //if so add nav.html contents to the top
+    if (container) {
+      container.prepend(data)
+      container.fadeIn();
+    }
+
   });
+
 });

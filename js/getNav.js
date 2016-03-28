f8218392bc1defc19e25a43249409ee6e90fcf8d
Andrew Bae <andrewbae94@gmail.com>
2016-03-27 20:39:59 -0400

    Huge Update

--- js/getNav.js
+++ js/getNav.js
@@ -1,17 +1,8 @@
-//get the file (replace with your own url)
-$.get("http://abae9.github.io/partials/nav.html", function(data) {
+$.get("http://pinkbun.github.io/partials/nav.html", function(data){
 
-  $(document).ready(function() {
+$(document).ready(function() {
 
-    //check if this document has .container
-    var container = $(".container");
-
-    //if so add nav.html contents to the top
-    if (container) {
-      container.prepend(data)
-      container.fadeIn();
-    }
-
-  });
-
-});
+     $(".container").prepend(data);
+     $(".container").fadeIn();
+     })
+})
